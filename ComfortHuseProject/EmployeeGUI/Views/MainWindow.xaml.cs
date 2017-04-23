d9bafbc97a4d3f7134591feb73d48cf35fb90321
alex855k <alexander2341@gmail.com>
2017-04-23 12:10:43 +0200

    sss

--- ComfortHuseProject/EmployeeGUI/Views/MainWindow.xaml.cs
+++ ComfortHuseProject/EmployeeGUI/Views/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace EmployeeGUI
         public MainWindow()
         {
             InitializeComponent();
+            List<ProductCategories> items = new List<ProductCategories>();
+            items.Add(new ProductCategories() { Title = "Housetype", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Carport/garage", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Plot", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Material(outside)", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Windows and doors", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Material(outside)", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Interior", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Floors", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Power", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Household appliances", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Tile", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Carpenter", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Builder", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Painter", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Plumbing", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Ventilation", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Plot expenses", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Additional construction expenses", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Extra Construction", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Other", Price = 0 });
+            items.Add(new ProductCategories() { Title = "Statement", Price = 0 });
+
+            ListBoxProductCatergories.ItemsSource = items;
         }
+
+        public class ProductCategories
+        {
+            public string Title { get; set; }
+            public int Price { get; set; }
+        }
+
     }
 }
