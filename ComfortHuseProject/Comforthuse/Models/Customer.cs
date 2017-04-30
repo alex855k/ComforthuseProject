e39253ae00c395723f6eba4593366491eb72ff07
alex855k <alexander2341@gmail.com>
2017-04-30 14:42:31 +0200

    Added some classes

---
+++ ComfortHuseProject/Comforthuse/Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Comforthuse.Models
+{
+    class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- ComfortHuseProject/ComforthuseUserstoryTests/UnitTest1.cs
+++ ComfortHuseProject/ComforthuseUserstoryTests/UnitTest1.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ComforthuseUserstoryTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UC04CreateCase
     {
         [TestMethod]
         public void TestMethod1()
